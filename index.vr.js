1cc00f62b0164c1f6d587a5258a055b22b49b0b1
VahanAper <asryanvahandev@gmail.com>
2017-05-23 15:59:13 +0400

    api initial setup

--- index.vr.js
+++ index.vr.js
@@ -6,11 +6,33 @@ import {
   Pano,
 } from 'react-vr';
 
+import API_KEY from './config';
+
+const URL = 'http://api.openweathermap.org/data/2.5/weather';
+const CITY = 'Yerevan';
+
 class WeatherSimulator extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      weatherObject: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      `${URL}?q=${CITY}&appid=${API_KEY}`,
+      { method: 'GET' },
+    )
+      .then(response => response.json())
+      .then(json => this.setState({ weatherObject: json }));
+  }
+
   render() {
     return (
       <View>
-        <Pano source={asset('lombard-vr.jpg')} />
+        <Pano source={asset('background.jpg')} />
       </View>
     );
   }
